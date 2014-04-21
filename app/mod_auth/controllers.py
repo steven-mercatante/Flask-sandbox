0a0a3784160350c1572383072ef0a147d217d91d
steven-mercatante <arrrms@gmail.com>
2014-04-20 22:08:21 -0400

    Working on user login, register

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -1,5 +1,6 @@
 from flask import abort, Blueprint, request, render_template, flash, g, \
 	session, redirect, url_for
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from werkzeug import check_password_hash, generate_password_hash
 
 from app import db
@@ -10,6 +11,14 @@ from app.mod_auth.models import User
 # Define the blueprint
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
+@app.before_request
+def before_request():
+	g.user = current_user
+
+@mod_auth.route('/home')
+def home():
+	return render_template('mod_auth/home.html', user=g.user)
+
 @mod_auth.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm(request.form)
@@ -17,9 +26,10 @@ def login():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user and check_password_hash(user.password, form.password.data):
 			session['user_id'] = user.id
-			flash('Welcome %s' % user.username)
-			redirect(url_for('auth.home'))
-		flash('Incorrect email or password', 'error')
+			return redirect(url_for('auth.home'))
+		else:
+			flash('Incorrect email or password', 'error')
+			return redirect(url_for('auth.login'))
 	return render_template('mod_auth/login.html', form=form)
 
 @mod_auth.route('/register', methods=['GET', 'POST'])
@@ -31,15 +41,13 @@ def register():
 		password = form.password.data
 		confirm_password = form.confirm_password.data
 		if not email or not password:
-			flash('Please provide an email address and password')
-			return redirect(url_for('auth.register'))
-		if password != confirm_password:
-			flash('Passwords must match')
+			flash('Please provide an email address and password', 'error')
 			return redirect(url_for('auth.register'))
 		if User.query.filter_by(email=email).first():
-			flash('That user account already exists')
+			flash('That user account already exists', 'error')
 			return redirect(url_for('auth.register'))
 		user = User(username=email, email=email, password=generate_password_hash(password))
 		db.session.add(user)
 		db.session.commit()
+		return redirect(url_for('auth.home'))
 	return render_template('mod_auth/register.html', form=form)
--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, BooleanField
-from wtforms.validators import Required, Email, EqualTo
+from wtforms.validators import Required, Email, EqualTo, Length
 
 
 class LoginForm(Form):
@@ -9,6 +9,12 @@ class LoginForm(Form):
 
 
 class RegisterForm(Form):
-	email = TextField('Email Address', [Email(), Required(message='Please enter a valid email address')])
-	password = PasswordField('Password', [Required(message='Please enter a password')])
-	confirm_password = PasswordField('Confirm Password', [Required(message='Please confirm your password')])
+	email = TextField('Email Address', [Email(), 
+		Required(message='Please enter a valid email address')])
+	password = PasswordField('Password', [
+		Required(message='Please enter a password'), 
+		Length(min=6, max=26, message='Password must be between %(min)d and %(max)d characters')])
+	confirm_password = PasswordField('Confirm Password', [
+		Required(message='Please confirm your password'), 
+		EqualTo(fieldname='password', message='Passwords must match'),
+		Length(min=6, max=26, message='Password must be between %(min)d and %(max)d characters')])
