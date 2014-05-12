5698d8ceef540a562b2ecd8bd91477ccf0c41b39
steven-mercatante <arrrms@gmail.com>
2014-05-11 22:48:53 -0400

    User model business logic

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
-from . import views
+from . import views, models
+from .. import login_manager
+
+login_manager.anonymous_user = models.AnonymousUser
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- app/auth/models.py
+++ app/auth/models.py
@@ -1,5 +1,13 @@
-from app import db
+from datetime import datetime
+import hashlib
+
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app, request, url_for
+from flask.ext.login import UserMixin, AnonymousUserMixin
+
+from app.exceptions import ValidationError
+from .. import db, login_manager
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -19,12 +27,14 @@ class Base(db.Model):
 class User(Base):
 	__tablename__ = 'user'
 
+	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(128), nullable=False)
 	email = db.Column(db.String(128), nullable=False, unique=True)
 	password_hash = db.Column(db.String(128), nullable=False)
 	role = db.Column(db.SmallInteger, nullable=False, default=ROLE_USER)
 	status = db.Column(db.SmallInteger, nullable=False, default=STATUS_PENDING)
-	is_activated = db.Column(db.SmallInteger, nullable=False, default=0)
+	confirmed = db.Column(db.Boolean, default=False)
+	last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
 	def __init__(self, username, email, password):
 		self.username = username
@@ -41,6 +51,65 @@ class User(Base):
 
 	def verify_password(self, password):
 		return check_password_hash(password)
+
+	def generate_confirmation_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'confirm': self.id})
+
+	def confirm(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+		    data = s.loads(token)
+		except:
+		    return False
+		if data.get('confirm') != self.id:
+		    return False
+		self.confirmed = True
+		db.session.add(self)
+		return True
+
+	def generate_reset_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'reset': self.id})
+
+	def reset_password(self, token, new_password):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+		    data = s.loads(token)
+		except:
+		    return False
+		if data.get('reset') != self.id:
+		    return False
+		self.password = new_password
+		db.session.add(self)
+		return True
+
+	def generate_email_change_token(self, new_email, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+	def change_email(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+		    data = s.loads(token)
+		except:
+		    return False
+		if data.get('change_email') != self.id:
+		    return False
+		new_email = data.get('new_email')
+		if new_email is None:
+		    return False
+		if self.query.filter_by(email=new_email).first() is not None:
+		    return False
+		self.email = new_email
+		self.avatar_hash = hashlib.md5(
+		    self.email.encode('utf-8')).hexdigest()
+		db.session.add(self)
+		return True
+
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
 
 	def is_authenticated(self):
 		if self.username:
@@ -60,3 +129,11 @@ class User(Base):
 
 	def __repr__(self):
 		return '<User %r>' % self.username
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
